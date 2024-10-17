f70a53a8abf4c98af94dfe1a9ae9dfde33d994bb
HenryHo <Henryliu926@gmail.com>
2024-10-17 14:35:36 -0500

    add basic script

---
+++ Script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+add() {
+	echo "The answer is: $(($1 + $2))"
+}
+subtract() {
+	echo "The answer is: $(($1 - $2))"
+}
+multiply() {
+	echo "The answer is: $(($1 * $2))"
+}
+divide() {
+	echo "The answer is: $(($1 / $2))"
+}
