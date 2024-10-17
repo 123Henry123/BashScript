29b6c9431d5526051c773c650834a43ceb0c6cf8
HenryHo <Henryliu926@gmail.com>
2024-10-17 14:42:33 -0500

    add operations

--- Script.sh
+++ Script.sh
@@ -11,3 +11,31 @@ multiply() {
 divide() {
 	echo "The answer is: $(($1 / $2))"
 }
+echo "Simple Calculator"
+echo "Select operation"
+echo "1) Addition"
+echo "2) Subtraction"
+echo "3) Multiplication"
+echo "4) Division"
+
+read -p "Enter selection [1-4]: " OPERATION
+
+read -p "Enter first number" NUM1
+read -p "Enter second number" NUM2
+case $OPERATION in
+	1)
+		add $NUM1 $NUM2
+		;;
+	2)
+		subtract $NUM1 $NUM2
+		;;
+	3)
+		multiplication $NUM1 $NUM2
+		;;
+	4)
+		division $NUM1 $NUM2
+		;;
+	*)
+		echo "Invalid choice"
+		;;
+	esac
